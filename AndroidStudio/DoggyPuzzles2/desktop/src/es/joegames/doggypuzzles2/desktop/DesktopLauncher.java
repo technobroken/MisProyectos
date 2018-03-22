4bf88501e01f18dd5922cca3f463092f5fc95133
technobroken <jose@localhost.localdomain>
2018-03-22 22:16:48 +0100

    AndroidStudio

---
+++ AndroidStudio/DoggyPuzzles2/desktop/src/es/joegames/doggypuzzles2/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package es.joegames.doggypuzzles2.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import es.joegames.doggypuzzles2.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
